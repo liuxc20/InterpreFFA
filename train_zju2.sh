023f20e6618c49f5bde6248505a27c12b4ed9b2a
liuxc20 <liuxc20@zju.edu.cn>
2024-11-02 15:01:51 +0800

    Update train_zju2.sh

--- train_zju2.sh
+++ train_zju2.sh
@@ -1,6 +1,6 @@
 python main_train.py \
---image_dir /data2/liuxiaocong/oph/RepGen-main/data/zju2/images/ \
---ann_path /data2/liuxiaocong/oph/RepGen-main/data/zju2/annotation.json \
+--image_dir /data/zju2/images/ \
+--ann_path /data/zju2/annotation.json \
 --dataset_name zju2 \
 --max_seq_length 40 \
 --use_jieba True \
@@ -8,7 +8,7 @@ python main_train.py \
 --threshold 3 \
 --batch_size 16 \
 --epochs 20 \
---save_dir results/zju2-36 \
+--save_dir results/zju2 \
 --step_size 10 \
 --gamma 0.1 \
 --contra_type 'base' \
@@ -18,5 +18,3 @@ python main_train.py \
 --contra_embed_size 256 \
 --contra_epoch 0 \
 --seed 36
-
-#--resume model/mimic_cxr.pth \
